227036ca9776c7e43211551ef071786478cdf9a7
polinaburchina <bur4inap@gmail.com>
2021-04-08 14:40:54 +0300

    1 lab is done

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,21 @@
 #include <jni.h>
 #include <string>
 
+#include <android/log.h>
+#define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "fclient_ndk", __VA_ARGS__)
+
+#include <spdlog/spdlog.h>
+#include "spdlog/sinks/android_sink.h"
+
+#define SLOG_INFO(...) android_logger->info( __VA_ARGS__ )
+auto android_logger = spdlog:: android_logger_mt(  "android",  "fclient_ndk");
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_iu3_rpo_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    LOG_INFO("Hellog from system log %d", 2021);
+    SLOG_INFO("Hello from spdlog {}", 2021);
     return env->NewStringUTF(hello.c_str());
 }
